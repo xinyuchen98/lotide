a452f7e12c355ef9d6555212c05cfa44c50a65b5
Xinyu Chen <xinyuchen98@gmail.com>
2022-01-12 02:02:27 +0000

    Created custom assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+// MY OWN TESTS
+assertEqual("Work", "Work");
+assertEqual("Work", "Walk");
+assertEqual(17, 17);
+assertEqual(17, 71);
