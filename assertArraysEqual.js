1ef2018dbe1a7c9980b8fd3cd0216db40e5add86
Xinyu Chen <xinyuchen98@gmail.com>
2022-01-13 00:13:02 +0000

    Completed assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (const i in array1) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], ["1", 2, 3]);
+assertArraysEqual([], []);
+assertArraysEqual([1, 2], [1, 2, 3]);
