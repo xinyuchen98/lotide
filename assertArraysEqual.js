4f6a08b4049da7cbfcd6b11869f1944cb521b2a7
Xinyu Chen <xinyuchen98@gmail.com>
2022-01-18 13:37:51 +0000

    Refactored assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (const i in array1) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-}
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -19,7 +8,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 }
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], ["1", 2, 3]);
-assertArraysEqual([], []);
-assertArraysEqual([1, 2], [1, 2, 3]);
+module.exports = assertArraysEqual;
