adcc13b67bf7241f34ed80415f8b511765406570
Xinyu Chen <xinyuchen98@gmail.com>
2022-01-18 13:45:59 +0000

    Refactored middle

--- middle.js
+++ middle.js
@@ -1,23 +1,5 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (const i in array1) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-}
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-  }
-}
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
 const middle = function (array) {
   const outputArr = [];
@@ -33,9 +15,4 @@ const middle = function (array) {
   return outputArr;
 }
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
